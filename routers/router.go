695b9630e0b1d386eb8e316b0445cb3c3f226481
Elijah <elijah.cyn@gmail.com>
2021-04-11 13:07:17 +0800

    article CRUD

--- routers/router.go
+++ routers/router.go
@@ -25,6 +25,17 @@ func InitRouter() *gin.Engine {
 		apiv1.PUT("/tags/:id", v1.EditTag)
 		// 刪除指定標籤
 		apiv1.DELETE("/tags/:id", v1.DeleteTag)
+
+		// 獲取文章列表
+		apiv1.GET("/articles", v1.GetArticles)
+		// 獲取指定文章
+		apiv1.GET("/articles/:id", v1.GetArticle)
+		// 新增文章
+		apiv1.POST("/articles", v1.AddArticle)
+		// 更新指定文章
+		apiv1.PUT("/articles/:id", v1.EditArticle)
+		// 刪除指定文章
+		apiv1.DELETE("/articles/:id", v1.DeleteArticle)
 	}
 
 	return  r
